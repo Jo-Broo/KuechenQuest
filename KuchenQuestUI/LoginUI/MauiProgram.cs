39b36fc3723818020ddb19222818eda22b32b1d8
ArisuSYG <niklasso272002@gmail.com>
2025-02-13 14:01:45 +0100

    Update UI and shit

--- KuchenQuestUI/LoginUI/MauiProgram.cs
+++ KuchenQuestUI/LoginUI/MauiProgram.cs
@@ -1,26 +1,41 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Net.Http;
 
 namespace LoginUI;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("ComicSansMS.ttf", "ComicSansMS");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("ComicSansMS.ttf", "ComicSansMS"); // Unverändert
+            });
 
-		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
+
+        // HttpClient für API-Anfragen registrieren
+        builder.Services.AddScoped(sp =>
+        {
+            var handler = new HttpClientHandler();
+
+            // Achtung: Deaktiviert SSL-Zertifikatsprüfung (nur für Entwicklung!)
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+
+            return new HttpClient(handler)
+            {
+                BaseAddress = new Uri("https://192.168.50.240:7067/KuechenQuest/") // Die URL der API
+            };
+        });
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
