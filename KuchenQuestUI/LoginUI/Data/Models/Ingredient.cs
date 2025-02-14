113dbcc47104e44bd2a7a22503725bad2191f674
muc952 <cornelius.mueller@hm-ag.de>
2025-02-14 12:39:23 +0100

    lokale db

---
+++ KuchenQuestUI/LoginUI/Data/Models/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUI.Data.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Image { get; set; }
+        public int Category { get; set; }
+        public Category? CategoryRef { get; set; }
+    }
+}
---
+++ KuchenQuestUI/LoginUI/Data/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUI.Data.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Time { get; set; }
+        public int Difficulty { get; set; }
+        public string? Instructions { get; set; }
+        public byte Rating { get; set; }
+        public int RatingCount { get; set; }
+        public string? Image { get; set; }
+        public Difficulty? DifficultyRef { get; set; }
+    }
+}
