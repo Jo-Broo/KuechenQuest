1d8a411f5659370b3976d37faf97c2bd8b49edb6
Jonas Wolf <Jonas.Wolf.1158@gmail.com>
2025-02-25 09:13:47 +0100

    Globalen Userservice implementiert

---
+++ KuchenQuestUI/LoginUI/Components/Service/UserService.cs
@@ -0,0 +1,14 @@
+﻿using LoginUI.Components.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUI.Components.Service
+{
+    public class UserService
+    {
+        public User CurrentUser { get; set; }
+    }
+}
